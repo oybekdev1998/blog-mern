e8aceaa421ac5576498e49996504ba8a0b82304b
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-10-21 15:43:41 +0500

    Fixed index file

--- server/index.js
+++ server/index.js
@@ -1,33 +1,71 @@
-import express from 'express'
-import jwt from 'jsonwebtoken'
+import express from 'express';
+import fs from 'fs';
+import multer from 'multer';
+import cors from 'cors';
+
+import mongoose from 'mongoose';
+
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
+
+import { handleValidationErrors, checkAuth } from './utils/index.js';
+
+import { UserController, PostController } from './controllers/index.js';
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err))
 
 const app = express()
 const PORT = 5000
-app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+const storage = multer.diskStorage({
+  destination: (_, _, cb) => {
+    if(!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
+const upload = multer({ storage });
 
-app.post('/auth/login', (req, res) => {
-  console.log(req.body)
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
 
-  const token = jwt.sign({
-    email: req.body.email,
-    fullName: 'Вася Пупкин',
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
 
-  }, 'secret123')
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
   res.json({
-    success: true
-  })
-})
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.get('/tags', PostController.getLastTags);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch(
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update,
+);
 
 
-app.listen(PORT, (err) => {
+app.listen(process.env.PORT | 5000, (err) => {
   if(err) {
     return console.log(err)
   }
-
   console.log(`Server has been started in port ${PORT}`)
 })
